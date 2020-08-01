04ae8ac891e5127667751398e3ebbc91e5371557
minecraft.zuphy@gmail.com <tankzu1016@gmail.com>
2020-08-01 13:42:29 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const Discord = require(`discord.js`);
+const fs = require(`fs`);
+
+const botSetting = require(`./settings.json`);
+
+const client = new Discord.Client();
+
+client.commands = new Discord.Collection();
+
+fs.readdir(`./commands/`, (err, files) => {
+  if(err) console.log(err);
+
+  let jsfile = files.filter(f => f.split(`.`).pop() === `js`)
+  if(jsfile.length <= 0){
+    console.log(`No commands found.`);
+    return;
+  }
+
+  jsfile.forEach((f, i) => {
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    client.commands.set(props.help.name, props);
+  });
+})
+
+client.once(`ready`, () => {
+  console.log(`Hunter's World is online!`);
+});
+
+client.on(`message`, message => {
+  if(!message.content.startsWith(botSetting.prefix) || message.author.bot) return;
+
+  let messageArray = message.content.split(` `);
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  let commandFile = client.commands.get(cmd.slice(botSetting.prefix.length));
+  if(commandFile) commandFile.run(client, message, args);
+});
+
+client.login(botSetting.token);
